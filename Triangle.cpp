88cce4481791a679e7ab3826d7c18cef0804c31a
rohanwetal <137687423+rohanwetal@users.noreply.github.com>
2024-01-28 19:25:56 +0530

    Add files via upload

---
+++ Triangle.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class TriangleSolution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        for (int i = triangle.size() - 2; i >= 0; --i)
+            for (int j = 0; j <= i; ++j)
+                triangle[i][j] += min(triangle[i + 1][j], triangle[i + 1][j + 1]);
+        return triangle[0][0];
+    }
+};
