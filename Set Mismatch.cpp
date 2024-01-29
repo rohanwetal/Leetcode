4af41e521c1bbbfe14776217de7a96b9ad09a543
rohanwetal <137687423+rohanwetal@users.noreply.github.com>
2024-01-29 21:12:00 +0530

    Add files via upload

---
+++ Set Mismatch.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <cmath>
+#include <stdexcept> 
+
+using namespace std;
+
+class ErrorFinder {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int duplicate;
+
+        for (const int num : nums) {
+            if (nums[abs(num) - 1] < 0)
+                duplicate = abs(num);
+            else
+                nums[abs(num) - 1] *= -1;
+        }
+
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] > 0)
+                return {duplicate, i + 1};
+        }
+
+        throw runtime_error("Error: No error found.");
+    }
+};
+
